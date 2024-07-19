c183eb0e409b347f387331f15d836a2f43ffb1d9
hetu <patelh29@myumanitoba.ca>
2024-07-18 19:45:56 -0500

    init gradle into the project

--- src/main/java/com/scheduler/application/Main.java
+++ src/main/java/com/scheduler/application/Main.java
@@ -1,7 +1,12 @@
 package com.scheduler.application;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Docker filer working");
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.println("You entered: \'" + str + "\'");
     }
 }
