867738c05b86d2fb7852fbf71af003705e744cff
hetu <patelh29@myumanitoba.ca>
2024-07-19 12:03:12 -0500

    final commit for init-project, I guess

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run: build
-	java -jar app/build/libs/app-1.0.0.jar
+	java -jar app/build/libs/app-1.0.0.jar --port=8081
 
 build:
 	./gradlew shadowJar
--- app/src/main/java/com/scheduler/application/Main.java
+++ app/src/main/java/com/scheduler/application/Main.java
@@ -1,12 +1,7 @@
 package com.scheduler.application;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Docker filer working");
-        Scanner s = new Scanner(System.in);
-        String str = s.nextLine();
-        System.out.println("You entered: \'" + str + "\'");
+        System.out.println("Hello World");
     }
 }
